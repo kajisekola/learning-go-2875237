a8f292745a5af27c8368471c078e4c09aff0bccf
kajisekola <39928528+kajisekola@users.noreply.github.com>
2022-03-05 15:09:02 -0500

    package main
    import (
    	"fmt"
    )

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
+<<<<<<< Updated upstream
 	fmt.Println("Structs")
+=======
+	anInt := 42
+	var p = &anInt
+	fmt.Println("Value of p:", *p)
+
+	// fmt.Println("Pointers")
+>>>>>>> Stashed changes
 }
